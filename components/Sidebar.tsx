fac89ad5d4f0fa714d7b45532125210d92dd3894
dilsher07singh <dilsher07singh@gmail.com>
2025-02-25 17:09:04 +0530

    Sidebar fixed and working

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import NewDocumentButton from "./NewDocumentButton";
+
+import { useCollection } from "react-firebase-hooks/firestore";
 import {
   Sheet,
   SheetContent,
@@ -7,12 +10,72 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+
 import { MenuIcon } from "lucide-react";
+import { useUser } from "@clerk/nextjs";
+import {
+  collectionGroup,
+  DocumentData,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "@/firebase";
+
+interface RoomDocument extends DocumentData {
+  createdAt: string;
+  role: "owner" | "editor";
+  roomId: string;
+  userId: string;
+}
 
 function Sidebar() {
+  const { user } = useUser();
+  const [groupedData, setGroupedData] = useState<{
+    owner: RoomDocument[];
+    editor: RoomDocument[];
+  }>({
+    owner: [],
+    editor: [],
+  });
+  console.log("USER IS", user);
+  const [data, loading, error] = useCollection(
+    user &&
+      query(
+        collectionGroup(db, "rooms"),
+        where("userId", "==", user.emailAddresses[0].toString())
+      )
+  );
+
+  useEffect(() => {
+    if (!data) return;
+
+    const grouped = data.docs.reduce<{
+      owner: RoomDocument[];
+      editor: RoomDocument[];
+    }>(
+      (acc, curr) => {
+        const roomData = curr.data() as RoomDocument;
+
+        if (roomData.role === "owner") {
+          acc.owner.push({ id: curr.id, ...roomData });
+        } else {
+          acc.editor.push({ id: curr.id, ...roomData });
+        }
+
+        return acc;
+      },
+      { owner: [], editor: [] }
+    );
+
+    setGroupedData(grouped);
+  }, [data]);
+
+  console.log("GRPUPED DATA IS", groupedData);
   const menuOptions = (
     <>
       <NewDocumentButton />
+
+      {/* My documents  */}
     </>
   );
   return (
