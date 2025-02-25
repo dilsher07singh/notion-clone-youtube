25c0a20b8e66de8cdf4e2719c5b5820585495ab8
dilsher07singh <dilsher07singh@gmail.com>
2025-02-25 15:24:07 +0530

    Basic sidebar and header implemenmted

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-
+import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -11,7 +12,15 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>{children}</body>
+        <body>
+          <Header />
+          <div className="flex min-h-screen">
+            <Sidebar />
+            <div className="flex-1 p-4 bg-gray-100 overflow-y-auto scrollbar-hide">
+              {children}{" "}
+            </div>
+          </div>
+        </body>
       </html>
     </ClerkProvider>
   );
