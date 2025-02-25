5149df70451bc07f1bf2ef27028bcac1a76a67ef
dilsher07singh <dilsher07singh@gmail.com>
2025-02-25 18:25:18 +0530

    Added hovering element to each sidebar option to show the current one

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,4 @@
 import { Button } from "@/components/ui/button";
 export default function Home() {
-  return (
-    <div>
-      <Button>Click over</Button>
-    </div>
-  );
+  return <div></div>;
 }
