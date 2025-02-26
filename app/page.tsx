626c898fc6cd9fb7bae0662c254b301fa73dc597
dilsher07singh <dilsher07singh@gmail.com>
2025-02-26 13:56:40 +0530

    Home page updated with a animating pulse

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { ArrowLeftCircle } from "lucide-react";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <main className="flex space-x-2 items-center animate-pulse">
+      <ArrowLeftCircle className="w-12 h-12" />
+      <h1 className="font-bold">Get started with creating a New Document</h1>
+    </main>
+  );
 }
