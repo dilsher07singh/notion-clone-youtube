e0dfad686f368236693a70ecfba619634b675de2
dilsher07singh <dilsher07singh@gmail.com>
2025-02-27 18:05:13 +0530

    Translations working and checked

--- components/TranslateDocument.tsx
+++ components/TranslateDocument.tsx
@@ -27,7 +27,9 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { LanguagesIcon } from "lucide-react";
+import { BotIcon, LanguagesIcon } from "lucide-react";
+import { toast } from "sonner";
+import Markdown from "react-markdown";
 
 type Language =
   | "english"
@@ -66,7 +68,29 @@ function TranslateDocument({ doc }: { doc: Y.Doc }) {
     e.preventDefault();
     // Send the question to the server
 
-    startTransition(async () => {});
+    startTransition(async () => {
+      const documentData = doc.get("document-store").toJSON();
+
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/translateDocument`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            documentData,
+            targetLang: language,
+          }),
+        }
+      );
+
+      if (res.ok) {
+        const { translated_text } = await res.json();
+        setSummary(translated_text);
+        toast.success("Translated Summary successfully!");
+      }
+    });
 
     // Close the dialog
   };
@@ -88,7 +112,21 @@ function TranslateDocument({ doc }: { doc: Y.Doc }) {
           </DialogDescription>
 
           <hr className="mt-5" />
+
+          {question && <p className="mt-5 text-gray-500">Q: {question}</p>}
         </DialogHeader>
+
+        {summary && (
+          <div className="flex flex-col items-start max-h-96 overflow-y-scroll gap-2 p-5 bg-gray-100">
+            <div className="flex">
+              <BotIcon className="w-10 flex-shrink-0" />
+              <p className="font-bold">
+                GPT {isPending ? "is thinking..." : "Says:"}
+              </p>
+            </div>
+            <p>{isPending ? "Thinking..." : <Markdown>{summary}</Markdown>}</p>
+          </div>
+        )}
 
         <form className="flex gap-2" onSubmit={handleAskQuestion}>
           <Select
@@ -98,6 +136,14 @@ function TranslateDocument({ doc }: { doc: Y.Doc }) {
             <SelectTrigger className="w-full">
               <SelectValue placeholder="Select a Language" />
             </SelectTrigger>
+
+            <SelectContent>
+              {languages.map((language) => (
+                <SelectItem key={language} value={language}>
+                  {language.charAt(0).toUpperCase() + language.slice(1)}
+                </SelectItem>
+              ))}
+            </SelectContent>
           </Select>
 
           <Button type="submit" disabled={!language || isPending}>
